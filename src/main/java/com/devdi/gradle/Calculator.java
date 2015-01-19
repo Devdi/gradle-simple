11352bf076874857d46678617359f50c29fa8fc4
Adam Szabo <adam_szabo1@epam.com>
2015-01-19 14:29:55 +0100

    add jUnit as dependency

---
+++ src/main/java/com/devdi/gradle/Calculator.java
@@ -0,0 +1,11 @@
+package com.devdi.gradle;
+
+/**
+ * Created by devdi on 1/19/15.
+ */
+public class Calculator {
+
+   public int add(int a, int b) {
+      return a + b;
+   }
+}
---
+++ src/test/java/com/devdi/gradle/CalculatorTest.java
@@ -0,0 +1,10 @@
+package com.devdi.gradle;
+
+public class CalculatorTest {
+
+   @org.junit.Test
+   public void test() {
+
+   }
+
+}
